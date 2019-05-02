c8077dc3879b2d76b9c859ee96fe83f0eaaac7ef
dianuchitop <48950463+dianuchitop@users.noreply.github.com>
2019-05-02 07:37:39 -0500

    Update graficas.py

--- graficas.py
+++ graficas.py
@@ -1,1 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt("Data.dat")
+grid = np.reshape(data, (101, 200))
+print(grid)
+
+
+plt.figure()
+plt.imshow(grid)
+plt.xlabel("Iteraccion")
+plt.ylabel("indice T")
+plt.colorbar(label="Temperatura")
+plt.savefig("plot.png")
 
